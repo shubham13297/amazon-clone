bb981d4c376bc06ad1aae606daaa2b6e4efda3ed
shubham <sy13021997@gmail.com>
2020-12-06 14:22:54 +0530

    Updates

---
+++ precache-manifest.8f9bab14911e4f7036c38e21d0736c03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb2985eeb8eb71ce96807f51bfea2ad",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "0d7fde67d1e1878baf19",
+    "url": "/amazon-clone/static/css/main.c8886d65.chunk.css"
+  },
+  {
+    "revision": "c8da702bd4a694aeeae2",
+    "url": "/amazon-clone/static/js/2.a7e558a1.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/amazon-clone/static/js/2.a7e558a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d7fde67d1e1878baf19",
+    "url": "/amazon-clone/static/js/main.b7993ffc.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
